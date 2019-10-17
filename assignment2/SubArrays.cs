5b545f9baa0b088db6376a9a1afff31555b7ea6d
spencervb256 <spencervb123@gmail.com>
2019-10-17 17:41:28 -0400

    SubArray changes

--- assignment2/SubArrays.cs
+++ assignment2/SubArrays.cs
@@ -3,14 +3,13 @@ namespace assignment2
 {
     public class SubArrays
     {
-        int[] intarray = new int[10];
 
 
         public void Find(int[] array1, int k)
         {
 
             int sum = 0;
-            
+
             //Fill array 1 through 10
             for (int i = 0; i < array1.Length; i++)
             {
@@ -19,17 +18,20 @@ namespace assignment2
             //This searches through the array and adds up numbers until it equals num > x
             for (int i = 0; i < array1.Length; i++)
             {
-                if (array1[i] + array1[i + 1] < k) // Out of bounds array
+                if (sum + array1[i] + array1[i + 1] < k) //Finds only one sub array, add in to find 2 different arrays such as {1, 2}, {2, 3}
+                                                         //both adding up to less than 6
                 {
                     sum += array1[i] + array1[i + 1];
+                    Console.ReadKey();
                 }
                 else
                 {
-
+                    Console.WriteLine("The sub array which adds up to " + k + "is " + "{" + array1[i] + "," + array1[i+1] + "}");
+                    break;
                 }
-                Console.WriteLine("The sub array which adds up to " + k + "is " + "{" + array1[i] + array1[i + 1] + "}");
             }
-                
+            
+
 
         }
     }
